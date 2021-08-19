9f443238735fef95fc22da69a295778ce7bb7217
Cole Moore <Cartisim@icloud.com>
2021-08-19 17:19:45 +0800

    updated checksum for ACBClientSDK 3.3.22-rc.1.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .target(
             name: "FCSDK-iOS",
             dependencies: ["ACBClientSDK", "CBARealTime", "SocketRocket"]),
-        .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.3.22.xcframework.zip", checksum: "40ecaa1a7f063c8212ec1be0d437f286026882d1fd61db8bc01d9f7d39023f5a"),
+        .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.3.22.xcframework.zip", checksum: "dd2a1cfeeebb1714fe8e13859342afa3198d484a4cc61e30b06712fc47605742"),
         .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m90.xcframework.zip", checksum: "f414904459259437ab1bbb457d670940d3277f8e71a1d36e9f0b8758910ee754"),
         .binaryTarget(name: "SocketRocket", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/socket_rocket/SocketRocket-0.3.3.xcframework.zip", checksum: "615c412b25f46c2824fae48f2f0c5ccddf9f38b499a5906d5d8a7f3a4ac7e6f9")
     ]
