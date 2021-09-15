4c36d3db5cdd64b57f039f7d3f741f30ba751fc7
Mike Beale <mike.beale@cba-japan.com>
2021-09-15 11:07:50 +0100

    Created 3.3.23 release candidate - expose provisional responses in new delegate method

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .target(
             name: "FCSDK-iOS",
             dependencies: ["ACBClientSDK", "CBARealTime"]),
-        .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.3.22.xcframework.zip", checksum: "dd2a1cfeeebb1714fe8e13859342afa3198d484a4cc61e30b06712fc47605742"),
+        .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.3.23-rc.1.0.xcframework.zip", checksum: "9c84bd7ff80dcb44316c1feceecacb866ab57d0b37253646c3d96ba99b5c2f0a"),
         .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m90.xcframework.zip", checksum: "f414904459259437ab1bbb457d670940d3277f8e71a1d36e9f0b8758910ee754")
     ]
 )
