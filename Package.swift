63d213aa561ed2222331ff73e5c4cee53dfa3c0a
Mike Beale <mike.beale@cba-japan.com>
2021-10-11 15:33:02 +0100

    3.4.0 Release

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .target(
             name: "FCSDK-iOS",
             dependencies: ["ACBClientSDK", "CBARealTime"]),
-        .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.3.23-rc.1.0.xcframework.zip", checksum: "9c84bd7ff80dcb44316c1feceecacb866ab57d0b37253646c3d96ba99b5c2f0a"),
+        .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.4.0.xcframework.zip", checksum: "5addde9ebe33e7d6d40be35c18c04934c8f8333d3aa1a27633f6be131b019a95"),
         .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m90.xcframework.zip", checksum: "f414904459259437ab1bbb457d670940d3277f8e71a1d36e9f0b8758910ee754")
     ]
 )
