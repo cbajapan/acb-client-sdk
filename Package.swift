b43987c73de396be6c7ea96a40feafb8ea152e3b
Cole Moore <Cartisim@icloud.com>
2021-11-12 19:34:07 +0800

    Slimmed binary for CBARealTime

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             name: "FCSDK-iOS",
             dependencies: ["ACBClientSDK", "CBARealTime"]),
         .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.4.0.xcframework.zip", checksum: "5addde9ebe33e7d6d40be35c18c04934c8f8333d3aa1a27633f6be131b019a95"),
-        .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m90.xcframework.zip", checksum: "f414904459259437ab1bbb457d670940d3277f8e71a1d36e9f0b8758910ee754")
+        .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m90-1.0.2.xcframework.zip", checksum: "4e492142aacf03676cea80b635ca40a1854b7bcda19027648c25b4e2ae249396")
     ]
 )
 
