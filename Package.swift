fd24b8242a46e5c1402c1eba399bdf8c8a45cb36
Cole Moore <Cartisim@icloud.com>
2022-03-03 15:57:38 +0800

    updated binary

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["ACBClientSDK", "CBARealTime"]),
     ],
     targets: [
-        .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.4.4-202202011351.xcframework.zip", checksum: "2b7826ba77c0eff03ed529e3ad45fb8ee5ad16fccad3c8813aa323f43f435ee7"),
+        .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.4.5.xcframework.zip", checksum: "1efe633746c6ed160e6f8032f5ffae23985238dde08f42d03c6249184e9ab10d"),
         .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m90-ios12-1.0.3.xcframework.zip", checksum: "e4943b29c8584ab5d0f4b3e38f4f3e914bd6b55f5dfe1066f27decc88d11387a")
     ]
 )
