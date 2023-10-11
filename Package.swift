e977b7528bf321370f6a3c7a8dfd277ad306a7a2
cerihughes <hughesceri+github@gmail.com>
2023-10-11 15:41:35 +0100

    Prepare release 3.4.6

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["ACBClientSDK", "CBARealTime"]),
     ],
     targets: [
-        .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.4.5.xcframework.zip", checksum: "1efe633746c6ed160e6f8032f5ffae23985238dde08f42d03c6249184e9ab10d"),
+        .binaryTarget(name: "ACBClientSDK", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/client_sdk/ACBClientSDK-3.4.6.xcframework.zip", checksum: "c38485b00b159b4f95b4b80968b4666ac270572a36316915f90a23d029784465"),
         .binaryTarget(name: "CBARealTime", url: "https://swift-sdk.s3.us-east-2.amazonaws.com/real_time/CBARealTime-m90-ios12-1.0.3.xcframework.zip", checksum: "e4943b29c8584ab5d0f4b3e38f4f3e914bd6b55f5dfe1066f27decc88d11387a")
     ]
 )
